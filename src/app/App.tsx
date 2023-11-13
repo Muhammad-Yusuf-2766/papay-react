8614ac27ab6b6e87a073dc82d1d55293643c6d52
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2023-11-13 20:06:25 +0900

    feat: develop routing based on papay project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,8 +8,14 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Users from './components/users';
-import Dishes from './components/dishes';
+// import Users from './components/users';
+// import Dishes from './components/dishes';
+import { RestaurantPage } from './screens/RestaurantPage';
+import { CommunityPage } from './screens/CommunityPage';
+import { OrdersPage } from './screens/OrdersPage';
+import { MemberPage } from './screens/MemberPage';
+import { HelpPage } from './screens/HelpPage';
+import { LoginPage } from './screens/LoginPage';
 
 
 function App() {
@@ -19,13 +25,25 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/restaurant">RestaurantPage</Link>
             </li>
             <li>
-              <Link to="/dishes">Dishes</Link>
+              <Link to="/community">CommunityPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">MemberPage</Link>
+            </li>
+            <li>
+              <Link to="/help">HelpPage</Link>
+            </li>
+            <li>
+              <Link to="/login">LoginPage</Link>
+            </li>
+            <li>
+              <Link to="/">HomePage</Link>
             </li>
           </ul>
         </nav>
@@ -33,17 +51,26 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/dishes">
-            <Dishes />
+          <Route path="/restaurant">
+            <RestaurantPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/community">
+            <CommunityPage />
+          </Route>
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <MemberPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
           </Route>
           <Route path="/">
-            <Container>
-              <Home />
-            </Container>
-            
+            <HomePage />  
           </Route>
         </Switch>
       </div>
@@ -55,6 +82,6 @@ function App() {
 export default App;
 
 
-function Home() {
-  return <h2>Home</h2>;
+function HomePage() {
+  return <Container>HomePage</Container>;
 }
