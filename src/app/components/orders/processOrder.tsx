8928149e8566505de1b7e8834cbd28c26dafc45b
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2023-11-25 19:03:59 +0900

    feat: Orders screen, develop orders page

---
+++ src/app/components/orders/processOrder.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TabPanel from "@mui/lab/TabPanel";
+
+const pausedOrders = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+];
+
+export default function ProcessOrders(props: any) {
+  return (
+    <TabPanel value="2">
+      <Stack>
+        {pausedOrders?.map((order, index) => {
+          return (
+            <Box className={"order_main_box"}>
+              <Box className={"order_box_scroll"}>
+                {order.map((item, itemIndex) => {
+                  const image_path = "/restaurant/recommendations_3.jpeg";
+                  return (
+                    <Box className={"ordersName_price"}>
+                      <img src={image_path} className={"orderDishImg"} alt="" />
+                      <p className={"titleDish"}>Turk Kebab</p>
+                      <Box className={"priceBox"}>
+                        <p>$7</p>
+                        <img src="/icons/Close.svg" alt="close" />
+                        <p>3</p>
+                        <img src="/icons/Pause.svg" alt="pause" />
+                        <p style={{ marginLeft: "15px" }}>$21</p>
+                      </Box>
+                    </Box>
+                  );
+                })}
+              </Box>
+              <Box className='process'>
+              <Box className={"total_price_box black_solid"}>
+                <Box className={"boxTotal"}>
+                  <p>mahsulot narxi</p>
+                  <p>$21</p>
+                  <img
+                    src="/icons/Plus.svg"
+                    alt="plus"
+                    style={{ marginLeft: "20px" }}
+                  />
+                  <p>yetkazish xizmati</p>
+                  <p>$2</p>
+                  <img
+                    src="/icons/pause.svg"
+                    alt="pause"
+                    style={{ marginLeft: "20px" }}
+                  />
+                  <p>jami narx</p>
+                  <p>$23</p>
+                </Box>
+                <Button
+                 variant="contained"
+                 style={{
+                  background:'#o288d1',
+                  color:'white',
+                  borderRadius:'10px'
+                 }} 
+                >
+                  Yakunlash
+                </Button>
+              </Box>
+              </Box>
+              
+            </Box>
+          );
+        })}
+      </Stack>
+    </TabPanel>
+  );
+}
--- src/app/screens/OrdersPage/index.tsx
+++ src/app/screens/OrdersPage/index.tsx
@@ -1,8 +1,83 @@
-import { Container } from "@mui/material";
-import React from 'react';
+import React, { useState } from "react";
+import { Box, Container, Stack } from "@mui/material";
+import "../../../css/orders.css";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PausedOrders from "../../components/orders/pausedOrders";
+import ProcessOrders from "../../components/orders/processOrder";
+import FinishedOrders from "../../components/orders/finishedOrders";
+import Marginer from "../../components/marginer";
 
 export function OrdersPage() {
-    return (
-        <Container>OrdersPage</Container>
-    );
+  // Initializations //
+  const [value, setValue] = useState("1");
+
+  // Handlers //
+  const handleChange = (event: any, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="order_page">
+      <Container
+        maxWidth="lg"
+        style={{ display: "flex", flexDirection: "row" }}
+        sx={{ mt: "50px", mb: "50px" }}
+      >
+        <Stack className="order_left">
+          <TabContext value={value}>
+            <Box className="order_nav_frame">
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <TabList
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Tab label="Buyurtmalarim" value="1" />
+                  <Tab label="Jarayon" value="2" />
+                  <Tab label="Yakunlangan" value="3" />
+                </TabList>
+              </Box>
+            </Box>
+            <Stack className="order_main_content">
+              <PausedOrders />
+              <ProcessOrders />
+              <FinishedOrders />
+            </Stack>
+          </TabContext>
+        </Stack>
+
+        <Stack className="order_right">
+          <Box className="order_info_box">
+            <Box className="order_user_img" />
+            <p className="order_user_name">Sami Yusuf</p>
+            <p className="order_user_prof">Foydalanuvchi</p>
+            <Box className="marginer"></Box>
+            <Box className="order_user_address">
+              <LocationOnIcon style={{ color: "#2E3A59" }} />
+              <p>Turkiye Istanbul</p>
+            </Box>
+          </Box>
+          <Box className='payment_box'>
+            <form action="#">
+                <input type="number" placeholder="Card number: " />
+                <Box className='card_mid_input'>
+                <input style={{width: "162px", marginRight:'10px    '}} type="date" placeholder="7/24: " />
+                <input style={{width: "162px"}} type="number" placeholder="CVV: 010: " />
+                </Box>
+                 <input type="text" placeholder="Full name:" />
+                <Box className='cards'>
+                    <a href="#"><img src="/icons/Western-union.svg" alt="" /></a>
+                    <a href="#"><img src="/icons/mastercard.svg" alt="" /></a>
+                    <a href="#"><img src="/icons/Paypal.svg" alt="" /></a>
+                    <a href="#"><img src="/icons/visa.svg" alt="" /></a>
+                </Box>
+            </form>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
